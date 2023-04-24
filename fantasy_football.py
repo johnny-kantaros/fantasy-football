2de45cb7f708164cf5396f0479314c08d04f5650
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-04-23 23:24:52 -0400

    merged dataframes

--- fantasy_football.py
+++ fantasy_football.py
@@ -1,3 +1,5 @@
+import pandas
+
 class Fantasy:
     def addContracts(self, df_raw, df_contracts):
         df_raw
@@ -9,6 +11,19 @@ class Fantasy:
         # for i in range(len(df_raw)):
         #     print(df_raw.loc[i, "Player"], df_raw.loc[i, "Team"])
 
-    def addAdvanced(self, df_raw, df_contracts):
-        for i in range(len(df_raw)):
-            df_contracts
+    def addAdvanced(self, df_raw, df_advanced):
+        # new data frame with split value columns
+        new = df_advanced["Player"].str.split(" ", n = 1, expand = True)
+
+        # making separate last name column from new data frame
+        df_advanced["Last Name"] = new[1]
+
+        raw_name = df_raw["Player"].str.split(" ", n = 1, expand = True)
+        
+        df_raw["Last Name"] = raw_name[1]
+
+        merged_df = df_raw.merge(df_advanced, how = 'inner', on = ['Last Name'])
+
+        # merged_df.drop("Last Name")
+
+        return merged_df
