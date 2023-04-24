5aa1450d7f3df61448528dc17bbee767caa5e9ec
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-04-23 21:07:34 -0400

    imported pro football reference

--- fantasy_football.py
+++ fantasy_football.py
@@ -1,5 +1,14 @@
 class Fantasy:
     def addContracts(self, df_raw, df_contracts):
-        # print(df_raw)
-        for i in df_raw:
-            print(i)
+        df_raw
+        # for i in range(len(df_contracts)):
+        #     for ii in range(len(df_raw)):
+                # print(i)
+                # if df_raw.loc[ii, "Player"].split()[1] == df_contracts.loc[i, "Player"].split()[1]:
+                    # print(df_contracts.loc[i, "Player"].split()[1])
+        # for i in range(len(df_raw)):
+        #     print(df_raw.loc[i, "Player"], df_raw.loc[i, "Team"])
+
+    def addAdvanced(self, df_raw, df_contracts):
+        for i in range(len(df_raw)):
+            df_contracts
