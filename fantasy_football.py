ac72d94f94d948ddbd35e946836e2b301342479c
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2023-04-24 22:51:40 -0400

    cleaaned up notebook

--- fantasy_football.py
+++ fantasy_football.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 
 class Fantasy:
     def addContracts(self, df_raw, df_contracts):
@@ -17,14 +17,10 @@ class Fantasy:
 
         # making separate last name column from new data frame
         df_advanced["Last Name"] = new[1]
-
         raw_name = df_raw["Player"].str.split(" ", n = 1, expand = True)
         
         df_raw["Last Name"] = raw_name[1]
-
         merged_df = df_raw.merge(df_advanced, how = 'inner', on = ['Last Name'])
-
-        # merged_df.drop("Last Name")
 
         return merged_df
     
@@ -32,7 +28,6 @@ class Fantasy:
         QB_data = df[df["Pos"] == "QB"]
         qb_drop_columns = ['Tgt', 'Rec', 'Receiving_Yds', 'Receiving_Td', 'Receiving_1st', 'Return_Yds', 'Return_Td', '2PT',]
         QB_data = QB_data.drop(qb_drop_columns, axis=1)
-        
 
         RB_data = df[df["Pos"] == "RB"]
         rb_drop_columns = [ 'Comp', 'Inc', 'Passing_Yds', 'Passing_Td', 'Int', 'Pic6', 'Sks', 'Passing_1st']
@@ -45,6 +40,18 @@ class Fantasy:
         TE_data = df[df["Pos"] == "TE"]
         te_drop_columns = ['Comp', 'Inc', 'Passing_Yds', 'Passing_Td', 'Int', 'Pic6', 'Sks', 'Passing_1st', 'Return_Yds', 'Return_Td']
         TE_data = TE_data.drop(te_drop_columns, axis=1)
-        
 
         return QB_data, RB_data, WR_data, TE_data
+    
+    def getAdvancedStats(self, type, year):
+        url_pro_football_reference = 'https://www.pro-football-reference.com/years/' + year + '/' + type + '_advanced.htm'
+
+        print(url_pro_football_reference)
+
+        table = pd.read_html(url_pro_football_reference, header=1)[0]
+
+        table = table.head(40)
+        table['Player'] = table['Player'].str.replace('*', '')
+        table['Player'] = table['Player'].str.replace('+', '')
+        
+        return table
