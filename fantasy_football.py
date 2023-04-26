9f3b7993191a9cf60116a1a4099a90d9d46f7c39
johnny kantaros <jkantaros@middlebury.edu>
2023-04-25 20:41:49 -0400

    updated documentation

--- fantasy_football.py
+++ fantasy_football.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 class Fantasy:
     def addContracts(self, df_raw, df_contracts):
@@ -46,12 +48,61 @@ class Fantasy:
     def getAdvancedStats(self, type, year):
         url_pro_football_reference = 'https://www.pro-football-reference.com/years/' + year + '/' + type + '_advanced.htm'
 
-        print(url_pro_football_reference)
 
-        table = pd.read_html(url_pro_football_reference, header=1)[0]
+        if type == "rushing":
 
-        table = table.head(40)
-        table['Player'] = table['Player'].str.replace('*', '')
-        table['Player'] = table['Player'].str.replace('+', '')
+            # For the rushing, we need to use selenium to account for the dynamically implemented dataset
+
+            # Set up ChromeDriver
+            driver = webdriver.Chrome()
+            driver.get(url_pro_football_reference)
+
+            # Wait for the page to load
+            driver.implicitly_wait(5)
+
+            # Get the page source
+            html = driver.page_source
+
+            # Parse the HTML with BeautifulSoup
+            soup = BeautifulSoup(html, 'html.parser')
+
+            # Find the table element by id
+            table = soup.find('table', {'id': 'advanced_rushing'})
+
+            # Quit the browser
+            driver.quit()
+
+            # Get columns and rows
+            headers = table.find_all('tr')[1].find_all('th') # Find all headers in HTML
+            players = table.find_all('tbody')[0] # Find all players (which will be our rows)
+
+
+            # Get column headers
+            cols = []
+            for th in headers:
+                cols.append(th.text.strip())
+            cols = cols[1:] # Dont want to include the row number
+
+            # Get table rows
+            rows = []
+
+            # Loop through each 'tr' tag
+            for tr in players.find_all('tr'):
+                row = []
+                for td in tr.find_all('td'):
+                    row.append(td.text.strip())
+                if row:
+                    rows.append(row)
+
+            # Make pandas df
+            table = pd.DataFrame(rows, columns=cols)
+            table['Player'] = table['Player'].str.replace('*', '')
+            table['Player'] = table['Player'].str.replace('+', '')
+
+        else:
+
+            table = pd.read_html(url_pro_football_reference, header=1)[0]
+            table['Player'] = table['Player'].str.replace('*', '')
+            table['Player'] = table['Player'].str.replace('+', '')
         
         return table
