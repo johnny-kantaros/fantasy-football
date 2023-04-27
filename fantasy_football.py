726fb09a861f4f71e9cdc96b6b4f50897b278954
johnny kantaros <jkantaros@middlebury.edu>
2023-04-27 12:31:04 -0400

    Broke up by position, fixed bugs

--- fantasy_football.py
+++ fantasy_football.py
@@ -3,6 +3,23 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 class Fantasy:
+
+
+    def read_data():
+
+        df = pd.read_excel('2022-offense.xlsx', sheet_name='Offense_Prior_Actuals', header=3)
+
+        # Drop columns that we don't need
+        drop_columns = ['GS', 'Bye', 'Notes', 'Rank', 'Y! Roto', 'Δ', 'Y! Old', 'Std', 'Δ.1', 'Std Old', 'PPR', 'Δ.2', 'PPR Old', '% Own', 'Fan Pts', 'PPG']
+        df = df.drop(drop_columns, axis=1)
+
+        # Change names of duplicate columns
+        updated_columns = {'Yds': 'Passing_Yds', 'Yds.1': 'Rushing_Yds', 'Yds.2': 'Receiving_Yds', 'Yds.3': 'Return_Yds', 
+                        'TD': 'Passing_Td', 'TD.1': 'Rushing_Td', 'TD.2': 'Receiving_Td', 'TD.3': 'Return_Td', 
+                        '1st': 'Passing_1st', '1st.1': 'Rushing_1st', '1st.2': 'Receiving_1st'}
+        df = df.rename(columns=updated_columns)
+        return df
+
     def addContracts(self, df_raw, df_contracts):
         df_raw
         # for i in range(len(df_contracts)):
@@ -15,18 +32,25 @@ class Fantasy:
 
     def addAdvanced(self, df_raw, df_advanced):
         # new data frame with split value columns
-        new = df_advanced["Player"].str.split(" ", n = 1, expand = True)
+        df_advanced['Player'] = df_advanced['Player'].apply(self.extract_initial_last_name)
 
-        # making separate last name column from new data frame
-        df_advanced["Last Name"] = new[1]
-        raw_name = df_raw["Player"].str.split(" ", n = 1, expand = True)
-        
-        df_raw["Last Name"] = raw_name[1]
-        merged_df = df_raw.merge(df_advanced, how = 'inner', on = ['Last Name'])
+        merged_df = df_raw.merge(df_advanced, on='Player', how='left').fillna(0)
 
         return merged_df
     
-    def makeRegularData(self, df):
+    def extract_initial_last_name(self, full_name):
+        name_parts = full_name.split(' ')
+        if len(name_parts) == 1:
+            return name_parts[0]
+        elif len(name_parts) == 2:
+            first_name, last_name = name_parts
+            return f"{first_name[0]}. {last_name}"
+        else:
+            return full_name
+
+
+    
+    def makeRegularData(self, df, position):
         QB_data = df[df["Pos"] == "QB"]
         qb_drop_columns = ['Tgt', 'Rec', 'Receiving_Yds', 'Receiving_Td', 'Receiving_1st', 'Return_Yds', 'Return_Td', '2PT',]
         QB_data = QB_data.drop(qb_drop_columns, axis=1)
@@ -43,19 +67,40 @@ class Fantasy:
         te_drop_columns = ['Comp', 'Inc', 'Passing_Yds', 'Passing_Td', 'Int', 'Pic6', 'Sks', 'Passing_1st', 'Return_Yds', 'Return_Td']
         TE_data = TE_data.drop(te_drop_columns, axis=1)
 
-        return QB_data, RB_data, WR_data, TE_data
+        if position == "QB": 
+            return QB_data
+        elif position == "RB":
+            return RB_data
+        elif position == "WR":
+            return WR_data
+        elif position == "TE":
+            return TE_data
+
     
     def getAdvancedStats(self, type, year):
         url_pro_football_reference = 'https://www.pro-football-reference.com/years/' + year + '/' + type + '_advanced.htm'
 
 
         if type == "rushing":
+            return self.get_table(url_pro_football_reference, "rushing")
+        
+        elif type == "receiving":
+            return self.get_table(url_pro_football_reference, "receiving")
 
-            # For the rushing, we need to use selenium to account for the dynamically implemented dataset
+        else:
 
-            # Set up ChromeDriver
+            table = pd.read_html(url_pro_football_reference, header=1)[0]
+            table['Player'] = table['Player'].str.replace('*', '')
+            table['Player'] = table['Player'].str.replace('+', '')
+        
+            return table
+    
+
+    def get_table(self, link, type):
+
+        # Set up ChromeDriver
             driver = webdriver.Chrome()
-            driver.get(url_pro_football_reference)
+            driver.get(link)
 
             # Wait for the page to load
             driver.implicitly_wait(5)
@@ -67,13 +112,19 @@ class Fantasy:
             soup = BeautifulSoup(html, 'html.parser')
 
             # Find the table element by id
-            table = soup.find('table', {'id': 'advanced_rushing'})
+            table = soup.find('table', {'id': f'advanced_{type}'})
 
             # Quit the browser
             driver.quit()
 
             # Get columns and rows
-            headers = table.find_all('tr')[1].find_all('th') # Find all headers in HTML
+
+            if type == "receiving":
+                headers = table.find_all('tr')[0].find_all('th') # Find all headers in HTML
+            else:
+                headers = table.find_all('tr')[1].find_all('th') # Find all headers in HTML
+
+            
             players = table.find_all('tbody')[0] # Find all players (which will be our rows)
 
 
@@ -98,11 +149,4 @@ class Fantasy:
             table = pd.DataFrame(rows, columns=cols)
             table['Player'] = table['Player'].str.replace('*', '')
             table['Player'] = table['Player'].str.replace('+', '')
-
-        else:
-
-            table = pd.read_html(url_pro_football_reference, header=1)[0]
-            table['Player'] = table['Player'].str.replace('*', '')
-            table['Player'] = table['Player'].str.replace('+', '')
-        
-        return table
+            return table
